dfed3f18ca0307ec1f4431fb773925ea50072d7a
ReekyStive <reekystive@gmail.com>
2023-11-02 04:42:19 +0800

    feat: ✨ add video autoplay

--- src/components/InfiniteScroller/Section.tsx
+++ src/components/InfiniteScroller/Section.tsx
@@ -10,37 +10,52 @@ const Section: FC<{
 }> = (props) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const entry = useIntersectionObserver(ref, {
-    threshold: [0.25],
+    threshold: [0.05, 0.5],
   });
   const isVisible = !!entry?.isIntersecting;
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    console.log(`Render Section ${props.id}`, { isVisible });
-    if (isVisible) {
-      props.onContentVisible?.();
+    console.log(`Render Section ${props.id} ${isVisible}, ${entry?.intersectionRatio}`, entry);
+    if (!entry) return;
+    if (entry.intersectionRatio > 0.3) {
+      if (entry.intersectionRatio > 0.5) {
+        console.log(`Render Section ${props.id} visible`);
+        props.onContentVisible?.();
+        void videoRef.current?.play();
+      } else {
+        console.log(`Render Section ${props.id} invisible`);
+        props.onContentInvisible?.();
+        videoRef.current?.pause();
+      }
     } else {
-      props.onContentInvisible?.();
+      if (!entry.isIntersecting) {
+        console.log(`Render Section ${props.id} fully invisible`);
+        videoRef.current?.currentTime && (videoRef.current.currentTime = 0);
+      } else {
+        console.log(`Render Section ${props.id} visible a bit`);
+      }
     }
-  }, [isVisible, props]);
+  }, [entry, isVisible, props]);
 
   return (
     <div
       className={
-        'flex h-full snap-center snap-always flex-col justify-center border-2 border-dashed border-blue-400' +
+        'relative flex h-full snap-center snap-always flex-col justify-center border-2 border-dashed border-blue-400' +
         ' ' +
         props.className
       }
       ref={ref}
     >
       <video
-        src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_5MB.mp4"
-        autoPlay
+        src="https://test-videos.co.uk/vids/jellyfish/mp4/h264/1080/Jellyfish_1080_10s_1MB.mp4"
         muted
-        controls
+        loop
         className="flex-1 object-cover"
         playsInline
+        ref={videoRef}
       ></video>
-      <div className="mx-auto py-2">{props.title}</div>
+      <div className="absolute flex h-full w-full flex-col items-center justify-center">{props.title}</div>
     </div>
   );
 };
