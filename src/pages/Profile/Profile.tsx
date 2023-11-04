bc642067c21fe49c958aa189f08c6be7b784a132
Mingxuan Wang <reekystive@gmail.com>
2023-11-04 21:33:50 +0800

    feat: ✨ optimize profile page ui

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,53 +2,25 @@ import { FC, useState } from 'react';
 import { IoPencil } from 'react-icons/io5';
 import styles from './Profile.module.scss';
 // import { useAppDispatch, useAppSelector } from '../../store/index.ts';
-const mock = [
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-  '../../../public/mock/avatar.png',
-];
+
+const mock = Array.from({ length: 100 }).map((_) => {
+  return '../../../public/mock/avatar.png';
+});
+
 const Profile: FC = () => {
   const [typeBarStyle, setTypeBar] = useState(
-    'relative left-[0%] h-[3px] w-[33%] bg-red-400 transition-all duration-300'
+    'relative left-[0%] h-[6px] w-[33%] bg-red-400 transition-all duration-300'
   );
   const chooseTypeBar = (type: string) => {
     return () => {
       console.log(type);
       if (type == 'videos') {
-        setTypeBar('relative left-[0%] h-[3px] w-[33%] bg-red-400 transition-all duration-300');
+        setTypeBar('relative left-[0%] h-[6px] w-[33%] bg-red-400 transition-all duration-300');
       } else if (type == 'favorites') {
         console.log('here');
-        setTypeBar('relative left-[33.5%] h-[3px] w-[33%] bg-red-400 transition-all duration-300');
+        setTypeBar('relative left-[33.5%] h-[6px] w-[33%] bg-red-400 transition-all duration-300');
       } else {
-        setTypeBar('relative left-[67%] h-[3px] w-[33%] bg-red-400 transition-all duration-300');
+        setTypeBar('relative left-[67%] h-[6px] w-[33%] bg-red-400 transition-all duration-300');
       }
     };
   };
@@ -57,47 +29,59 @@ const Profile: FC = () => {
   // const dispatch = useAppDispatch();
 
   return (
-    <div className="flex flex-col overflow-y-auto px-0 sm:px-[20px]">
-      <div className="m-[10px] flex h-[130px] w-full flex-row items-center gap-2 p-[5px]">
-        <img src="/mock/avatar.png" alt="avatar" className="aspect-square h-[90%] rounded-full object-cover" />
-        <div className="flex h-full flex-col justify-between overflow-hidden p-3">
+    <div className="mx-auto flex h-[100dvh] max-w-6xl flex-col overflow-y-auto">
+      <div className="flex w-full flex-row items-center gap-3 p-[15px] pt-[100px] sm:pt-[180px]">
+        <img
+          src="/mock/avatar.png"
+          alt="avatar"
+          className="aspect-square h-[120px] rounded-full object-cover sm:h-[150px]"
+        />
+        <div className="flex h-full flex-col justify-center overflow-hidden">
           <div className="select-text overflow-hidden text-ellipsis whitespace-nowrap break-all text-3xl font-bold">
-            LucausMartinLucausMartinLucausMartinLucausMartin
+            Lorem ipsum dolor sit amet
           </div>
-          <div className="select-text overflow-hidden text-ellipsis whitespace-nowrap break-all p-[2px] text-lg font-medium">
+          <div className="ms-[2px] line-clamp-2 select-text text-ellipsis break-all text-lg font-medium">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quo nesciunt voluptate beatae, voluptatem,
             rem architecto illum corrupti ratione animi perferendis vitae inventore ea blanditiis explicabo similique
             itaque dolore corporis.
           </div>
-          <button className="outline-white-500 mt-[10px] flex w-[150px] flex-row items-center justify-center gap-2 rounded-md px-3 py-0 outline outline-2">
+          <button className="border-white-500 mt-[10px] flex w-fit flex-row items-center justify-center gap-2 rounded-md border-2 px-3 py-1">
             <IoPencil className="pt-[1px]" size={22}></IoPencil>
             <span className="text-xl">Edit Profile</span>
           </button>
         </div>
       </div>
-      <div className="flex flex-col gap-1 p-4 text-lg">
+      <div className="flex flex-col gap-1 p-4">
         <div className="flex flex-row gap-3 text-lg">
-          <span>0 Following</span>
-          <span>0 Followers</span>
-          <span>0 Likes</span>
+          <span>
+            <span className="font-bold">192</span> Following
+          </span>
+          <span>
+            <span className="font-bold">233</span> Followers
+          </span>
+          <span>
+            <span className="font-bold">128</span> Likes
+          </span>
         </div>
         <div>
           <span>No bio yet.</span>
         </div>
       </div>
-      <div className="flex max-w-6xl flex-1 select-none flex-col overflow-x-hidden pt-3">
-        <div className="flex flex-row items-center justify-around">
-          <button className="w-full bg-transparent text-xl" onClick={chooseTypeBar('videos')}>
-            Videos
-          </button>
-          <button className="w-full bg-transparent text-xl" onClick={chooseTypeBar('favorites')}>
-            Favorites
-          </button>
-          <button className="w-full bg-transparent text-xl" onClick={chooseTypeBar('likes')}>
-            Likes
-          </button>
+      <div className="debug-outline flex flex-1 select-none flex-col overflow-x-clip">
+        <div className="sticky top-0 h-fit bg-white dark:bg-[#242424]">
+          <div className="flex flex-row items-center justify-around">
+            <button className="w-full bg-transparent py-2" onClick={chooseTypeBar('videos')}>
+              Videos
+            </button>
+            <button className="w-full bg-transparent py-2" onClick={chooseTypeBar('favorites')}>
+              Favorites
+            </button>
+            <button className="w-full bg-transparent py-2" onClick={chooseTypeBar('likes')}>
+              Likes
+            </button>
+          </div>
+          <div className={typeBarStyle}></div>
         </div>
-        <div className={typeBarStyle}></div>
         <div className="w-full pb-[56px] sm:pb-0">
           <div className={`${styles['grid-container']} w-full flex-1`}>
             {mock.map((item, index) => {
