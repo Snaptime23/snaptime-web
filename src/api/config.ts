7067b307dd613d4f927abc204e92a5c369413f55
Mingxuan Wang <reekystive@gmail.com>
2023-11-05 23:58:44 +0800

    feat: ✨ update api base url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,7 @@
-const baseUrl = 'http://10.210.175.112:3005';
+// prod
+// const baseUrl = 'https://service-b8fdfydl-1253954317.sh.apigw.tencentcs.com/release';
+
+// dev
+const baseUrl = 'https://service-m973oigf-1253954317.sh.apigw.tencentcs.com/release';
 
 export { baseUrl };
