70782c7cd24ac7b69a98bc3889e78f72f4c103e1
Mingxuan Wang <reekystive@gmail.com>
2023-11-05 16:16:21 +0800

    feat: ✨ add react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -11,12 +13,17 @@ if (!rootDOM) {
   throw new Error('Root DOM not found');
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(rootDOM).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
